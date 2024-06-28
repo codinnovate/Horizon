59de0eb776923a13d4f71356ad010d334ef9b293
codinnovate <adeyemis710@gmail.com>
2024-06-28 12:15:06 +0100

    first commit

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client'
+import CountUp from 'react-countup';
+
+
+const AnimatedCounter = ({amount} : {amount:number}) => {
+    return (
+        <div className='w-full'>
+            <CountUp
+                decimals={2}
+                prefix='$'
+                decimal=','
+                end={amount}
+            />
+        </div>
+    )
+}
+
+export default AnimatedCounter
